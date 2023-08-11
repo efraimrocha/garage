688e9932a7e238bbb93e5144d4d42e593b1e134e
Efraim Rocha <67542881+efraimrocha@users.noreply.github.com>
2023-08-11 14:41:08 -0300

    Adição de fotos, novas rotas, e templates.
    - Adição de fotos testes.
    - Adição da API do Google Maps - mas inda não foi colocada a chve.

--- app/routes.py
+++ app/routes.py
@@ -22,5 +22,19 @@ def rider():
 @app.route('/contato')
 def contato():
     return render_template('contato.html')
-    
-    
+
+@app.route('/sobre-nos')  #api_key=YOUR_GOOGLE_MAPS_API_KEY)
+def sobre_nos():
+    return render_template('sobre-nos.html')
+
+#@app.route('/login')
+#def membro_login():
+#    return render_template('user_login.html')
+
+#@app.route('/cadastro')
+#def membro_cadastro():
+#    return render_template('cadastro.html')
+
+#@app.route('/perfil')
+#def perfil():
+#    return render_template*('perfil_')
